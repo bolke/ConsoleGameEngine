3e7c69cb6cb1299fd9d7db248055a9d97020d065
renko <r.de.boer@yp-yourpartner.nl>
2018-12-03 11:50:57 +0100

    exe running demo and lib

---
+++ CGEConsole/Program.cs
@@ -0,0 +1,34 @@
+﻿using CGEDemoLib.Pong;
+using CGELib.Engines;
+using System.Threading;
+
+namespace Pong
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GameEngine game = new GameEngine();
+            game.VisualEngine = new ConsoleEngine();            
+            
+            game.Start();
+
+            PongScene scene = new PongScene();
+
+            scene.Initialize();
+
+            Thread.Sleep(1000);
+
+            game.VisualEngine.RendersPerSecond = 60;
+            game.UpdatesPerSecond = 30;
+
+            game.VisualEngine.Scene = scene;
+
+            while (game.IsRunning)
+            {
+                Thread.Sleep(1000);
+            }
+
+        }
+    }
+}
