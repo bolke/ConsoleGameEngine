ad9845eba9a165507127f571a35d2fe462886464
renko <r.de.boer@yp-yourpartner.nl>
2018-12-12 10:39:08 +0100

    cleanup, all into square

--- CGELib/Sprites/Shapes/Actors/SquareActor.cs
+++ CGELib/Sprites/Shapes/Actors/SquareActor.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CGELib.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,30 +7,8 @@ using System.Threading.Tasks;
 
 namespace CGELib.Sprites.Shapes.Actors
 {
-    public abstract class SquareActor: ActingSprite
+    public abstract class SquareActor: Square, IActor
     {
-        public virtual Point TopLeft { get; set; } = new Point(0,0);
-        public virtual Point BottomRight { get; set; } = new Point(0, 0);
-
-        public int Width { get; set; }
-        public int Height { get; set; }
-
-        public virtual int Color { get; set; }
-
-        public override bool Initialize()
-        {
-            return true;
-        }
-
-        public override bool Render(ConsoleOutput screen)
-        {
-            TopLeft.X = Center.X - Width / 2;
-            TopLeft.Y = Center.Y - Height / 2;
-            BottomRight.X = Center.X + Width / 2;
-            BottomRight.Y = Center.Y + Height / 2;
-
-            screen.Rectangle(TopLeft, BottomRight, Color);
-            return true;
-        }
+        public abstract bool Update(long tick);
     }
 }
