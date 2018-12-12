e7c6d87342a89f47d577e8faf5db055d72eaae7d
renko <r.de.boer@yp-yourpartner.nl>
2018-12-12 10:38:01 +0100

    added ivisual to sprite

--- CGELib/Sprites/Sprite.cs
+++ CGELib/Sprites/Sprite.cs
@@ -1,16 +1,18 @@
 ﻿using CGELib.Interfaces;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CGELib.Sprites
 {
     public abstract class Sprite : ISprite
     {
-        public virtual Point Center {get; set; } = new Point(0, 0);
+        private int _width = 0;
+        private int _height = 0;
 
+        public virtual Point Center {get; set; } = new Point(0, 0);
+        public int Width { get { return (int)Math.Round(_width * Scale); } set { _width = value; } }
+        public int Height { get { return (int)Math.Round(_height * Scale); } set { _height = value; } }
+        public double Scale { get; set; }
+        public double Rotation { get; set; } = 0; 
         public abstract bool Render(ConsoleOutput screen);
         public abstract bool Initialize();
     }
