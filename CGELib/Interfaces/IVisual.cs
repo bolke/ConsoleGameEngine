4c7b19a23162774ba724b95681182358c70a3f17
renko <r.de.boer@yp-yourpartner.nl>
2018-12-12 10:36:23 +0100

    base for a visual component, contains center, width, height, scale, rotation

---
+++ CGELib/Interfaces/IVisual.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CGELib.Interfaces
+{
+    public interface IVisual
+    {
+        int Width { get; set; }
+        int Height { get; set; }
+        double Scale { get; set; }
+        double Rotation { get; set; }
+    }
+}
